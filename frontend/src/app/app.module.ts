26cb8c17e56d0711e042ba13f44642fa75ad1c67
fede-palazz <fede.palazzi1999@gmail.com>
2023-02-02 11:29:18 +0100

    IOT-40 First navbar responsive attempt

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/nav/navbar/navbar.component';
 import { SidebarComponent } from './components/nav/sidebar/sidebar.component';
+import { SmartNavComponent } from './components/nav/smart-nav/smart-nav.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, SidebarComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    SmartNavComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ frontend/src/app/components/nav/smart-nav/smart-nav.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-smart-nav',
+  templateUrl: './smart-nav.component.html',
+  styleUrls: ['./smart-nav.component.scss'],
+})
+export class SmartNavComponent {}
