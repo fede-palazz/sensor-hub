e129cd2e28067793cca173ef615140148a15fcc3
fede-palazz <fede.palazzi1999@gmail.com>
2023-02-10 09:55:34 +0100

    IOT-49 Created systems component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/nav/navbar/navbar.component';
 import { SidebarComponent } from './components/nav/sidebar/sidebar.component';
 import { MenuComponent } from './components/nav/menu/menu.component';
+import { SystemsComponent } from './components/systems/systems/systems.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, SidebarComponent, MenuComponent],
+  declarations: [AppComponent, NavbarComponent, SidebarComponent, MenuComponent, SystemsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ frontend/src/app/components/systems/systems/systems.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-systems',
+  templateUrl: './systems.component.html',
+  styleUrls: ['./systems.component.scss']
+})
+export class SystemsComponent {
+
+}
