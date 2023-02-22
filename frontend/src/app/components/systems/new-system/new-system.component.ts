7d104feed2087f0188ad11e9043423bfbc175b80
fede-palazz <fede.palazzi1999@gmail.com>
2023-02-22 13:25:37 +0100

    IOT-49 Added system panel preview

--- frontend/src/app/components/systems/new-system/new-system.component.ts
+++ frontend/src/app/components/systems/new-system/new-system.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Button } from 'src/app/model/button';
+import { System } from 'src/app/model/system/system';
 
 @Component({
   selector: 'app-new-system',
@@ -7,7 +8,7 @@ import { Button } from 'src/app/model/button';
   styleUrls: ['./new-system.component.scss'],
 })
 export class NewSystemComponent {
-  buttons: Button[] = [
+  titleButtons: Button[] = [
     {
       icon: 'arrow_back',
       text: 'Impianti',
@@ -26,7 +27,6 @@ export class NewSystemComponent {
   ];
 
   colorsList = [
-    'is-black',
     'is-dark',
     'is-primary',
     'is-link',
@@ -37,13 +37,39 @@ export class NewSystemComponent {
   ];
   selectedIconIndex?: number;
   selectedColorIndex?: number;
+  system: System;
+
+  constructor() {
+    this.selectedIconIndex = 0;
+    this.selectedColorIndex = 0;
+    // Initialize preview system
+    this.system = {
+      name: '',
+      color: this.colorsList[0],
+      icon: this.iconsList[0],
+      masterNodes: [
+        {
+          id: '1',
+          name: 'Nodo Master 1',
+          slaveNodes: [
+            { id: '1', name: 'nodo slave 1' },
+            { id: '2', name: 'nodo slave 2' },
+            { id: '3', name: 'nodo slave 3' },
+            { id: '4', name: 'nodo slave 4' },
+          ],
+        },
+      ],
+    };
+  }
 
   onIconClick(index: number): void {
     this.selectedIconIndex = index;
+    this.system.icon = this.iconsList[this.selectedIconIndex];
   }
 
   onColorClick(index: number): void {
     this.selectedColorIndex = index;
+    this.system.color = this.colorsList[this.selectedColorIndex];
   }
 
   onSubmit(): void {
